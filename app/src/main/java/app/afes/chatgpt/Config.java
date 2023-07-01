a04dade0dd147ed3189b5f6dcfd4c56648b0e83d
HP 21 <espitiaa321@gmail.com>
2023-07-01 11:33:52 -0500

    u

---
+++ app/src/main/java/app/afes/chatgpt/Config.java
@@ -0,0 +1,9 @@
+package app.afes.chatgpt;
+
+public class Config {
+
+    //Cargar la API_KEY de OpenAI
+    //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
+    //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
+    public static final String CHATAPI_KEY = "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";
+}
